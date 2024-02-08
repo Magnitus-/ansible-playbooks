c2445fd90a087aa57a2bf46e21fda5e77197a768
Eric Vallee <eric_vallee@webificservices.com>
2024-02-08 00:33:33 -0500

    Added flags to install rgw and mds. Made rgw pools setup idempotent and added existing pools class adjustment wherever it is supported

--- roles/ceph/crush_rules/templates/setup_crush_rules.sh
+++ roles/ceph/crush_rules/templates/setup_crush_rules.sh
@@ -2,15 +2,17 @@
 
 set -e
 
-{% if has_ssd %}
+{% if use_device_classes and has_ssd %}
 if [ -z "$(ceph osd crush rule ls | grep replicated_ssd | { grep -v grep || test $? = 1; })" ]; then
   ceph osd crush rule create-replicated replicated_ssd default host ssd
 fi
 {% endif %}
 
+{% if use_device_classes %}
 if [ -z "$(ceph osd crush rule ls | grep replicated_hdd | { grep -v grep || test $? = 1; })" ]; then
   ceph osd crush rule create-replicated replicated_hdd default host hdd
 fi
+{% endif %}
 
 if [ -z "$(ceph osd pool get .mgr crush_rule | grep {{ mgr_crush_rule }} | { grep -v grep || test $? = 1; })"]; then
   ceph osd pool set .mgr crush_rule {{ mgr_crush_rule }}
